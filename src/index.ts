dc5d34e1bbdd5178365a174be296eaf7c5cb2741
Soso <soso.beriashvili@redberry.ge>
2024-10-20 14:01:04 +0400

    feat: install uuid package

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import http from "node:http";
+import http from 'node:http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-type", "text/plain");
-  res.write("<h1>hello World!</h1>");
+  res.setHeader('Content-type', 'text/plain');
+  res.write('<h1>hello World!</h1>');
   res.end();
 });
 
-server.listen(3000, () => console.log("Running on PORT 3000!"));
+server.listen(3000, () => console.log(`Running on PORT ${port}!`));
